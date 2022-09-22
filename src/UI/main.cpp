a9ddcb490cb9d7917ee094cd163dcd0e6df5f93d
Danila Sidorov <a4036389@gmail.com>
2022-09-23 00:21:45 +0300

    change font on windows

--- src/UI/main.cpp
+++ src/UI/main.cpp
@@ -9,6 +9,11 @@ int main(int argc, char* argv[])
 
     app.setOrganizationDomain("https://github.com/danila258/Vega");
     app.setApplicationName("Vega");
+    
+    #if defined(Q_OS_WIN)
+        QFont serifFont("Times", 14);
+        QApplication::setFont(serifFont);
+    #endif
 
     MainWidget* Vega = new MainWidget();
     Vega->show();
